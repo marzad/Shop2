b94050fe4270f3ffad88f3e49158375b6a3d7505
marzad <maschatz@gmx.net>
2022-11-08 12:41:57 +0100

    Initial commit

---
+++ src/test/java/de/neuefische/marzad/shop2/Shop2ApplicationTests.java
@@ -0,0 +1,13 @@
+package de.neuefische.marzad.shop2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Shop2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
