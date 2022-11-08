be9804f34665e7e65da8f09e49e1fec1761fbf14
marzad <maschatz@gmx.net>
2022-11-08 15:28:10 +0100

    Changed Order to Lombok

--- src/main/java/de/neuefische/marzad/shop2/model/Order.java
+++ src/main/java/de/neuefische/marzad/shop2/model/Order.java
@@ -1,18 +1,23 @@
 package de.neuefische.marzad.shop2.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Order {
-    private List<Product> products;
+
     private int id;
+    private List<Product> products;
 
-    public Order() {
-    }
-
-    public Order(int id, List<Product> products) {
+/*    public Order(int id, List<Product> products) {
         this.products = products;
         this.id = id;
-    }
+    }*/
 
     public List<Product> getProducts() {
         return products;
